18bb3e80285b458312575456392d9ef5f5b26a0a
Madhavan Raja <madhavanraja99@gmail.com>
2023-02-01 22:14:49 -0700

    Cleaned code

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,7 @@ fn main() {
     let args = CoenArgs::parse();
 
     match args.operation {
-        New(new_args) => {
-            Coen::create(new_args);
-        }
-        Build(file) => {
-            println!("Building current project: {file:?}");
-        }
+        New(new_args) => Coen::create(new_args),
+        Build(file) => println!("Building current project: {file:?}"),
     }
 }
