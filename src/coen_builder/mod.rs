689852e1f9b85f63915377f8a8d7565525893c18
Madhavan Raja <madhavanraja99@gmail.com>
2023-02-08 22:55:37 -0700

    Cleaned code

--- src/coen_builder/mod.rs
+++ src/coen_builder/mod.rs
@@ -2,8 +2,6 @@ use std::{collections::HashSet, error::Error, fs, path::PathBuf};
 
 use indexmap::IndexMap;
 
-use crate::args::CoenArgs;
-
 #[derive(Debug)]
 pub struct CoenBuilder {
     target: Option<PathBuf>,
@@ -31,34 +29,6 @@ impl CoenBuilder {
             conversion_files: HashSet::new(),
             current_statement: String::new(),
         })
-    }
-
-    fn get_root_path(path: &PathBuf) -> Result<PathBuf, Box<dyn Error>> {
-        let mut root_path = fs::canonicalize(&path)?;
-
-        if root_path.is_dir() {
-            root_path.push("root.coen");
-
-            while !root_path.exists() {
-                root_path.pop();
-
-                if !root_path.pop() {
-                    return Err("Cannot locate root file.".into());
-                }
-
-                root_path.push("root.coen");
-            }
-        }
-
-        Ok(root_path)
-    }
-
-    pub fn get_content(&self) -> String {
-        self.content.clone()
-    }
-
-    pub fn get_target(&self) -> Option<PathBuf> {
-        self.target.clone()
     }
 
     pub fn write(&self, target_override: Option<PathBuf>) -> Result<(), Box<dyn Error>> {
